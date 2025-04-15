fb5e470f78077169f92457f4055a580e3f5b8b45
ds-code-t <dscode80@proton.me>
2025-04-15 16:17:46 -0700

    updated LegacyScenario to take TestCaseState

--- src/main/java/io/pickleball/cucumberutilities/LegacyScenario.java
+++ src/main/java/io/pickleball/cucumberutilities/LegacyScenario.java
@@ -2,35 +2,43 @@ package io.pickleball.cucumberutilities;
 
 
 import cucumber.api.Result;
-import io.cucumber.java.Scenario;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class LegacyScenario implements cucumber.api.Scenario {
-    private final Scenario delegate;
+import cucumber.api.Scenario;
+import io.cucumber.core.runner.TestCaseState;
+import java.util.ArrayList;
 
-    public LegacyScenario(Scenario scenario) {
-        this.delegate = scenario;
+
+public class LegacyScenario implements Scenario {
+    private final TestCaseState testCaseState;
+
+    public LegacyScenario(TestCaseState testCaseState) {
+        this.testCaseState = testCaseState;
     }
 
     @Override
     public Collection<String> getSourceTagNames() {
-        return delegate.getSourceTagNames();
+        return testCaseState.getSourceTagNames();
     }
 
     @Override
     public Result.Type getStatus() {
-        switch (delegate.getStatus()) {
-            case FAILED:
-                return Result.Type.FAILED;
+        switch (testCaseState.getStatus()) {
             case PASSED:
                 return Result.Type.PASSED;
+            case FAILED:
+                return Result.Type.FAILED;
             case SKIPPED:
                 return Result.Type.SKIPPED;
             case PENDING:
+                return Result.Type.PENDING;
             case UNDEFINED:
                 return Result.Type.UNDEFINED;
+            case AMBIGUOUS:
+                return Result.Type.AMBIGUOUS;
             default:
                 return Result.Type.UNDEFINED;
         }
@@ -38,37 +46,37 @@ public class LegacyScenario implements cucumber.api.Scenario {
 
     @Override
     public boolean isFailed() {
-        return delegate.isFailed();
+        return testCaseState.isFailed();
     }
 
     @Override
     public void embed(byte[] data, String mimeType) {
-        delegate.attach(data, mimeType, "Embedded Data");
+        testCaseState.attach(data, mimeType, "Embedded Data");
     }
 
     @Override
     public void write(String text) {
-        delegate.log(text);
+        testCaseState.log(text);
     }
 
     @Override
     public String getName() {
-        return delegate.getName();
+        return testCaseState.getName();
     }
 
     @Override
     public String getId() {
-        return delegate.getId();
+        return testCaseState.getId();
     }
 
     @Override
     public String getUri() {
-        return delegate.getUri().toString();
+        return testCaseState.getUri().toString();
     }
 
     @Override
     public List<Integer> getLines() {
-        Integer line = delegate.getLine();
-        return line != null ? Collections.singletonList(line) : Collections.emptyList();
+        Integer line = testCaseState.getLine();
+        return line != null ? Collections.singletonList(line) : new ArrayList<>();
     }
 }
