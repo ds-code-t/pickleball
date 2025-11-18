69df9729e70e08ce3785190d02c3d14b17b3d535
ds-code-t <dscode80@proton.me>
2025-11-18 10:18:16 -0700

    add set step

--- src/main/java/tools/dscode/coredefinitions/NavigationSteps.java
+++ src/main/java/tools/dscode/coredefinitions/NavigationSteps.java
@@ -13,15 +13,7 @@ import static tools.dscode.coredefinitions.GeneralSteps.getBrowser;
 
 public class NavigationSteps {
 
-    @When("^navigate to: (.*)$")
-    public void i_navigate_to(String text) {
-        text = "configs." + text;
-        System.out.println("@@i_navigate_to: " + text + "");
-        WebDriver driver = (WebDriver) getScenarioObject("browser");
-        System.out.println("@@driver: " + driver + "");
-        StepExtension stepExtension = getCurrentScenarioState().getCurrentStep();
-        System.out.println("@@before Get ");
-        System.out.println("@@before Get=getAndResolve " + stepExtension.getStepParsingMap().getAndResolve(text));
-        driver.get((String) stepExtension.getStepParsingMap().getAndResolve(text));
+    @When("set {returnStepParameter}")
+    public void setBrowser(String config) {
     }
 }
