2ea17bd65f2382e03d8018ed52c0de6677fb950a
ds-code-t <dscode80@proton.me>
2026-01-05 08:09:39 -0700

    testing

--- src/main/java/tools/dscode/common/treeparsing/xpathcomponents/XPathyBuilder.java
+++ src/main/java/tools/dscode/common/treeparsing/xpathcomponents/XPathyBuilder.java
@@ -31,7 +31,7 @@ public final class XPathyBuilder {
         XPathy base = XPathy.from("//" + t);
         Op o = (op == null) ? Op.DEFAULT : op;
         String attr = (xPathyAttr == null) ? null :  xPathyAttr.toString();
-        if(!attr.startsWith("@")) attr = "@" + attr;
+        if(attr != null && !attr.startsWith("@")) attr = "@" + attr;
         // null ValueWrapper + (EQUALS/DEFAULT) => presence-only (attr) or no constraint (text)
         if (v == null && (o == Op.DEFAULT || o == Op.EQUALS)) {
             return (attr == null) ? base : XPathy.from("//" + t + "[" + attr + "]");
